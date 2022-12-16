04930912ceea98e25748bf2b4484673935a7d3de
Serkan <ipeks8926@gmail.com>
2022-12-17 00:55:33 +0300

    Add project files.

---
+++ RecapProject1/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapProject1
+{
+   public class NorthwindContext : DbContext
+   {
+        public DbSet<Entities.Product> Products { get; set; }
+        public DbSet<Entities.Category> Categories { get; set; }
+   }
+}
